c972175544bb13fb5b07e65988a78e3701278433
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2025-10-24 11:34:21 +0600

    Farmers

--- Farmers.c
+++ Farmers.c
@@ -2,45 +2,21 @@
 #include<string.h>
 
 int main(){
-    int a[3],b[3],c[3],d[3];
+
+    int t;
+    scanf("%d",&t);
+while (t--)
+{
+    int a[3];
    for (int i = 0; i <3; i++)
     {
         scanf("%d",&a[i]);
     }
-
-
-     for (int i = 0; i <3; i++)
-    {
-        scanf("%d",&b[i]);
-    }
-
-
-     for (int i = 0; i <3; i++)
-    {
-        scanf("%d",&c[i]);
-    }
-
-
-     for (int i = 0; i <3; i++)
-    {
-        scanf("%d",&d[i]);
-    }
-        int m1=a[2]*a[0]/a[2];
-        int result1=a[2]-m1;
-
-        int m2=b[2]*b[0]/b[2];
-        int result2=m2-b[2];
-
-        int m3=c[2]*c[0]/c[2];
-        int result3=m3-c[2];
-
-        int m4=d[2]*d[0]/d[2];
-        int result4=d[2]-m4;
-
-        printf("%d\n",result1);
-        printf("%d\n",result2);
-        printf("%d\n",result3);
-        printf("%d\n",result4);
-
+    int sum=a[0]+a[1];
+    int mul=a[0]*a[2];
+    float d=mul/sum;
+    float result=a[2]-d;
+     printf("%0.f\n",result);
+}
  return 0;
 }
