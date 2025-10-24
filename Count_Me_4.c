8f744ed532ae8574c94682b6b55b135b7bae9ca1
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2025-10-24 16:02:37 +0600

    Count_Me_4

--- Count_Me_4.c
+++ Count_Me_4.c
@@ -2,21 +2,37 @@
 #include<string.h>
 
 int main(){
-    char a[100001];
+    char a[10000];
     scanf("%s",a);
     int length=strlen(a);
 
-    for (int i = 0; i < length; i++)
-    {
-        printf("%c",a[i]);
-    }
 
-    int f[10000]={0};
-for (int i = 0; i < 10000; i++)
+// int f[length];
+// for (int i = 0; i < length; i++)
+// {
+//     f[i]=0;
+// }
+
+int f[26]={0};
+for (int i = 0; i < length; i++)
 {
-    f[a[i]]++;
+    if (a[i]>='a' && a[i]<='z' )
+    {
+         int su=a[i]-'a';
+
+    f[su]++; 
+    }  
+ 
 }
-printf("%d",f);
+
+for (int i = 0; i <26; i++)
+{
+    if (f[i]>0)
+    {
+        printf("%c - %d\n",i+'a',f[i]);
+    }   
+}
+
 
 return 0; 
 }
