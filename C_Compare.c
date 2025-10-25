42e11970cfc486fbc04b3d8dc84200a7ea740a5e
kamrulhasananik019 <kamrulhasananik019@gmail.com>
2025-10-25 10:11:55 +0600

    C_Compare

---
+++ C_Compare.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+    char x[10000],y[10000];
+    scanf("%s %s",x,y);
+    int i=0;
+    while (1)
+    { if (x[i]=='\0'&& y[i]=='\0')
+        {
+        printf("%s",y);
+        break;
+        }else if (x[i]>y[i])
+        {
+            printf("%s",y);
+              break;
+        }else if (x[i]<y[i])
+        {
+           printf("%s",x);
+             break;
+        }else if (x[i]=='\0')
+        {
+            printf("%s",x);
+            break;
+        }else if (y[i]=='\0')
+        {
+            printf("%s",y);
+            break;
+        }
+            
+i++;
+    }
+    return 0;
+}
+
+
